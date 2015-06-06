124fd64f908fbb0ecc9e4b829b2e888ac330d475
cnoffsin <cnoffsin@gmail.com>
2015-06-06 19:02:16 -0400

    Fail OpenStack API endpoints chaos types

---
+++ src/main/java/com/netflix/simianarmy/chaos/FailKeystoneChaosType.java
@@ -0,0 +1,19 @@
+package com.netflix.simianarmy.chaos;
+
+import com.netflix.simianarmy.MonkeyConfiguration;
+
+/**
+ * Fail all Keystone API endpoints for Openstack.
+ */
+public class FailKeystoneChaosType extends FailOpenstackEndpointChaosType {
+
+    /**
+     * Get the right end point across to FailOpenstackEndpointChaosType.
+     *
+     * @param config
+     *            Configuration settings
+     */
+    public FailKeystoneChaosType(final MonkeyConfiguration config) {
+        super(config, "nova");
+    }
+}
